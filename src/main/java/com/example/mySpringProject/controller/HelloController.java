1b5ce2c921c08d56b5486d643e9c92d9affa2bf9
codinglone <codinglone@gmail.com>
2023-06-25 17:04:05 +0400

    Added another route

--- src/main/java/com/example/mySpringProject/controller/HelloController.java
+++ src/main/java/com/example/mySpringProject/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.mySpringProject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,11 @@ public class HelloController {
     @GetMapping("/helloworld")
     public String helloWorld(){
         return "Hello There!!";
+    }
+
+    @RequestMapping("/cars")
+    public String cars(){
+        return "Volvo, Honda";
     }
 
 }
