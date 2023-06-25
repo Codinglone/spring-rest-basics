e7da0da62e910306224379224d4a1d66fe407df0
codinglone <codinglone@gmail.com>
2023-06-25 16:40:50 +0400

    Initial commit

---
+++ src/main/java/com/example/mySpringProject/MySpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mySpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mySpringProject/MySpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mySpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
