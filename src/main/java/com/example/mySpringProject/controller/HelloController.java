799856788feb57c73d812f1ded74071b6a00fe1f
codinglone <codinglone@gmail.com>
2023-06-26 14:46:44 +0400

    Added Base path functionality

--- src/main/java/com/example/mySpringProject/controller/HelloController.java
+++ src/main/java/com/example/mySpringProject/controller/HelloController.java
@@ -3,8 +3,11 @@ package com.example.mySpringProject.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
+@RequestMapping("/app")
 public class HelloController {
     @GetMapping("/helloworld")
     public String helloWorld(){
